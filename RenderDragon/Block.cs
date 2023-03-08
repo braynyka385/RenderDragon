f316bcf103ae4e4e0a55a414372253294f49f918
braynyka385 <braynyka385@ed.amdsb.ca>
2023-03-08 08:44:13 -0500

    trhjsfdhtsrhbdrhnhtdthhrasgd

---
+++ RenderDragon/Block.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderDragon
+{
+    class Block
+    {
+        private int pos = new int[3];
+    }
+}
--- RenderDragon/Player.cs
+++ RenderDragon/Player.cs
@@ -8,10 +8,10 @@ namespace RenderDragon
 {
     internal class Player
     {
-        UInt16 cameraDirectionX = UInt16.MaxValue/2;
+        UInt16 cameraDirectionX = UInt16.MaxValue / 2;
         UInt16 cameraDirectionY = UInt16.MaxValue / 2;
 
-        double[] pos = new double[4]; //x,y,z,w; might remove w later...
+        double[] pos = new double[3]; //x,y,z
         public Player(double x, double y, double z)
         {
 
